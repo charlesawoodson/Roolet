073f41cb21301871d1e3ad3a863cefa773d721d0
Charles Woodson <charlesawoodson@gmail.com>
2021-01-20 18:54:05 -0500

    Added contacts activity

--- app/src/main/java/com/charlesawoodson/roolet/MainActivity.kt
+++ app/src/main/java/com/charlesawoodson/roolet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.charlesawoodson.roolet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.charlesawoodson.roolet.contacts.CreateGroupActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.addGroupImageView).setOnClickListener {
+            Intent(this, CreateGroupActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/charlesawoodson/roolet/contacts/CreateGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.charlesawoodson.roolet.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.charlesawoodson.roolet.R
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_group)
+    }
+}
