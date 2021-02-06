56e6c094c0b3a5dbf38ac39e78c9a501d986381d
Charles Woodson <CharlesAWoodson@gmail.com>
2021-02-06 16:37:43 -0500

    Roolet settings improvements

--- app/src/main/java/com/charlesawoodson/roolet/settings/SettingsFragment.kt
+++ app/src/main/java/com/charlesawoodson/roolet/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.charlesawoodson.roolet.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,11 @@ import com.charlesawoodson.roolet.R
 import com.charlesawoodson.roolet.mvrx.BaseFragment
 import kotlinx.android.synthetic.main.fragment_contacts.*
 
-class SettingsFragment : BaseFragment() {
+class SettingsFragment : BaseFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private val sharedPreferences by lazy(mode = LazyThreadSafetyMode.NONE) {
+        requireActivity().getSharedPreferences(getString(R.string.preference_file_key), 0)
+    }
 
     private val viewModel: SettingsViewModel by fragmentViewModel()
 
@@ -28,9 +33,35 @@ class SettingsFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // if
+
         backImageView.setOnClickListener {
             requireActivity().finish()
         }
+    }
+
+    companion object {
+
+    }
+
+    /**
+     * Called when a shared preference is changed, added, or removed. This
+     * may be called even if a preference is set to its existing value.
+     *
+     *
+     * This callback will be run on your main thread.
+     *
+     *
+     * *Note: This callback will not be triggered when preferences are cleared via
+     * [Editor.clear].*
+     *
+     * @param sharedPreferences The [SharedPreferences] that received
+     * the change.
+     * @param key The key of the preference that was changed, added, or
+     * removed.
+     */
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        TODO("Not yet implemented")
     }
 
 }
