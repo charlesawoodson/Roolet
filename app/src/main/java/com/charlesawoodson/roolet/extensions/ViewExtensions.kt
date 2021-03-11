44f63f0c70accbbd6d2e426b30b1a1b209f5b6ec
Charles Woodson <CharlesAWoodson@gmail.com>
2021-03-11 14:50:56 -0500

    Added AndroidX Toolbar for GroupsActivity and inflated menu resource

--- app/src/main/java/com/charlesawoodson/roolet/extensions/ViewExtensions.kt
+++ app/src/main/java/com/charlesawoodson/roolet/extensions/ViewExtensions.kt
@@ -10,11 +10,15 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils.loadLayoutAnimation
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.AnimRes
 import androidx.annotation.ColorRes
 import androidx.annotation.LayoutRes
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.charlesawoodson.roolet.R
 
 fun ViewGroup.inflate(@LayoutRes resId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(resId, this, attachToRoot)
@@ -40,6 +44,20 @@ fun ImageView.setDrawableTint(@ColorRes colorRes: Int) {
             ContextCompat.getColor(context, colorRes),
             PorterDuff.Mode.MULTIPLY
         )
+}
+
+fun Toolbar.changeToolbarFont() {
+    for (i in 0 until childCount) {
+        val view = getChildAt(i)
+        if (view is TextView && view.text == title) {
+            view.typeface = ResourcesCompat.getFont(
+                context,
+                R.font.chuck_five_font
+            )
+
+            break
+        }
+    }
 }
 
 /**
--- app/src/main/java/com/charlesawoodson/roolet/groups/GroupsActivity.kt
+++ app/src/main/java/com/charlesawoodson/roolet/groups/GroupsActivity.kt
@@ -1,20 +1,56 @@
 package com.charlesawoodson.roolet.groups
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.charlesawoodson.roolet.R
+import com.charlesawoodson.roolet.extensions.changeToolbarFont
+import kotlinx.android.synthetic.main.activity_container.*
 
 class GroupsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
+        setSupportActionBar(toolbar)
+        toolbar.changeToolbarFont()
+
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<GroupsFragment>(R.id.container)
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add_group -> {
+                Toast.makeText(applicationContext, "Add Group Action", Toast.LENGTH_LONG).show()
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(applicationContext, "Settings Action", Toast.LENGTH_LONG).show()
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
             }
         }
     }
